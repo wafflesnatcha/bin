a40cb123a627d6e883bc8f6447b4658ac37aa3ba
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-11 01:32:08 -0800

    no message

--- Darwin/google_chrome_sort_bookmarks.sh
+++ Darwin/google_chrome_sort_bookmarks.sh
@@ -3,9 +3,9 @@
 osascript -e 'activate application "Google Chrome"'
 for(( i=0; i<20; i++ )); do
 	cliclick 238 162
-	sleep 0.3
+	sleep 0.2
 	cliclick 287 411
 	sleep 0.2
 	osascript -e 'tell application "System Events" to tell process "Google Chrome" to key code 125'
-	sleep 0.3
+	sleep 0.2
 done
