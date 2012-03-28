27c4aed3844c9b43acc86a411675938857bb1acf
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-27 23:07:08 -0700

    * benchmark.sh: now accepts short option values without a space in between (see d9cf9ff55f689b52216a470a032ff525434a03d5)

--- Darwin/benchmark.sh
+++ Darwin/benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_NAME="benchmark.sh"
-SCRIPT_VERSION="0.3.8 (2012-03-12)"
+SCRIPT_VERSION="0.3.9 (2012-03-27)"
 
 opt_delay=0
 opt_iterations=2
@@ -13,8 +13,8 @@ Benchmark a shell script.
 Usage: ${0##*/} [options] [command]
 
 Options:
- -d, --delay SECONDS    Seconds to wait in between executions (default ${opt_delay})
- -i, --iterations NUM   Number of iterations to run (default ${opt_iterations})
+ -d, --delay SECONDS    Seconds to wait in between executions (${opt_delay})
+ -i, --iterations NUM   Number of iterations to run (${opt_iterations})
  -h, --help             Show this help
 EOF
 }
@@ -37,17 +37,19 @@ make_cmd() {
 	chmod +x "$TMPCMD"
 }
 
-line_back() {
-	echo -n R | tr 'R' '\r';
-}
-
 TIME_BIN=$(which time 2>/dev/null) || FAIL "time program not found"
 
 while (($#)); do
 	case $1 in
 		-h|--help) usage; exit 0 ;;
-		-d|--delay) opt_delay=$2; shift ;;
-		-i|--iterations) opt_iterations=$2; shift ;;
+		-d*|--delay)
+			[[ $1 =~ ^\-[a-z].+$ ]] && opt_delay="${1:2}" || { opt_delay=$2; shift; }
+			{ echo -n "$opt_delay" | grep -E "^[0-9]{0,}\.?[0-9]{1,}$" &>/dev/null; } || FAIL "invalid delay"
+		;;
+		-i*|--iterations)
+			[[ $1 =~ ^\-[a-z].+$ ]] && opt_iterations="${1:2}" || { opt_iterations=$2; shift; }
+			[[ ! $opt_iterations =~ ^[0-9]+$ || $opt_iterations < 1 ]] && FAIL "invalid iterations"
+		;;
 		-*|--*) FAIL "unknown option ${1}" ;;
 		*) break ;;
 	esac
@@ -62,7 +64,7 @@ for (( i = 1; i <= $opt_iterations; i++ )); do
 	[[ $i > 1 ]] && sleep $opt_delay
 	seconds=$({ "$TIME_BIN" "$TMPCMD"; } 2>&1 | tail -n1 | sed -E 's/^[ ]*([^ ]*).*$/\1/')
 	echo "$seconds"
-	[[ $seconds ]] && total_time=$(echo $total_time $seconds | awk '{print ($1+$2)}')
+	[[ $seconds ]] && total_time=$(echo $total_time $seconds | awk '{print $1+$2}')
 done
 
 average_time=$(echo $total_time $opt_iterations | awk '{printf "%.2f", $1/$2}')
