85b90b7652212f20da4aa47a6ceead517d44c1b5
Scott Buchanan <buchanan.sc@gmail.com>
2012-05-07 14:49:57 -0700

    no message

--- Darwin/mac.sh
+++ Darwin/mac.sh
@@ -1,228 +1,229 @@
 #!/usr/bin/env bash
 SCRIPT_NAME="mac.sh"
-SCRIPT_VERSION="1.0.2 2012-04-03"
-FAIL() { [[ $1 ]] && echo "$SCRIPT_NAME: $1" >&2; [[ ! $2 = -1 ]] && exit ${2:-1}; }
+SCRIPT_DESC="Do stuff with OS X like changing settings and shit."
+SCRIPT_VERSION="1.0.3 2012-05-04"
+
+ERROR() { [[ $1 ]] && echo "$SCRIPT_NAME: $1" >&2; [[ $2 > -1 ]] && exit $2; }
 
 pref_bool() {
-	case "$(echo $2 | tr '[A-Z]' '[a-z]')" in
+	case "$(echo $2 | tr '[:upper:]' '[:lower:]')" in
 		y|yes|1|true|on) defaults write $1 -bool TRUE ;;
 		n|no|0|false|off|nay) defaults write $1 -bool FALSE ;;
 		*) [[ $(defaults read $1 2>/dev/null) = 1 ]] && { echo "on"; return 1; } || { echo "off"; return 2; } ;;
 	esac
-	return
 }
 
 pref_float() {
-	if [[ ! $2 ]]; then
-		v=$(defaults read $1 2>&1) && { echo $v; return 1; } || { echo "not set"; return 2; }
-	elif [[ $2 = "-" ]]; then
-		defaults delete $1
-	else
-		defaults write $1 -float $2
+	if [[ ! $2 ]]; then v=$(defaults read $1 2>&1) && { echo $v; return 1; } || { echo "not set"; return 2; }
+	elif [[ $2 = "-" ]]; then defaults delete $1
+	else defaults write $1 -float $2
 	fi
-	return
 }
 
-finder_restart() {
-	osascript -e 'tell application "Finder" to quit' -e 'try' -e 'tell application "Finder" to reopen' -e 'on error' -e 'tell application "Finder" to launch' -e 'end try'
-	return
-}
+mac() {
+	for (( i = 0 ; i <= ${#@} ; i++ )); do
+		eval local ARG${i}=${!i}
+		eval local lARG${i}="$(echo ${!i} | tr '[:upper:]' '[:lower:]')"
+	done
 
-run() {
-	case $1 in
+	case $lARG1 in
 
-	d|dock) shift
-	case $1 in
+	dock|d) shift
+	case $lARG2 in
 
-		addspace)
-		defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}' && killall Dock
-		;;
-
-		dimhidden)
-		pref_bool "com.apple.dock showhidden" $2 && killall Dock
-		;;
-		
-		noglass)
-		pref_bool "com.apple.dock no-glass" $2 && killall Dock
-		;;
-
-		restart)
-		killall Dock
-		;;
-
-		*)
-		cat <<-EOF
-		Usage: ${0##*/} dock ...
-		 addspace            Add a spacer to the dock
-		 dimhidden [on/off]  Hidden applications appear dimmer on the dock
-		 noglass [on/off]    Toggle the 3d display of the dock
-		 restart             Reload the dock
+		--usage) cat <<-EOF
+		addspace            Add a spacer to the dock
+		dimhidden [on/off]  Hidden applications appear dimmer on the dock
+		noglass [on/off]    Toggle the 3d display of the dock
+		restart             Reload the dock
 		EOF
-		return 1
+		;;
+
+		addspace) defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}' && killall Dock
+		;;
+
+		dimhidden) pref_bool "com.apple.dock showhidden" $2 && killall Dock
+		;;
+
+		noglass) pref_bool "com.apple.dock no-glass" $2 && killall Dock
+		;;
+
+		restart) killall Dock
+		;;
+
+		*) [[ $1 ]] && ERROR "unknown command $ARG1 $ARG2"; mac help $ARG1; return 1
 		;;
 
 	esac
 	;;
 
-	e|expose) shift
-	case $1 in
+	expose|e) shift
+	case $lARG2 in
 
-		anim-duration)
-		pref_float "com.apple.dock expose-animation-duration" $2 && killall Dock
+		--usage) cat <<-EOF
+		anim-duration [FLOAT/-]  Expose (Mission Control) animation duration
+		EOF
 		;;
 
-		*)
-		cat <<-EOF
-		Usage: ${0##*/} expose ...
-		 anim-duration [FLOAT/-]  Expose (Mission Control) animation duration
-		EOF
-		return 1
+		anim-duration) pref_float "com.apple.dock expose-animation-duration" $2 && killall Dock
+		;;
+
+		*) [[ $1 ]] && ERROR "unknown command $ARG1 $ARG2"; mac help $ARG1; return 1
 		;;
 
 	esac
 	;;
 
-	f|finder) shift
-	case $1 in
+	finder|f) shift
+	case $lARG2 in
 
-		showhidden)
-		pref_bool "com.apple.finder AppleShowAllFiles" $2 && finder_restart
-		;;
-
-		fullpathview)
-		pref_bool "com.apple.finder _FXShowPosixPathInTitle" $2
-		;;
-
-		r|restart)
-		finder_restart
-		;;
-
-		sf|showfile) shift
-		[[ ! $(type -p setfile) && $(type -p chflags) ]] && cmd="chflags nohidden" || cmd="setfile -a v"
-		for f in "$@"; do [[ -e "$f" ]] && $cmd "$f" || FAIL "file doesn't exist: $f" -1; done
-		;;
-
-		hf|hidefile) shift
-		[[ ! $(type -p setfile) && $(type -p chflags) ]] && cmd="chflags hidden" || cmd="setfile -a V"
-		for f in "$@"; do [[ -e "$f" ]] && $cmd "$f" || FAIL "file doesn't exist: $f" -1; done
-		;;
-
-		*)
-		cat <<-EOF
-		Usage: ${0##*/} finder ...
-		 showfile PATH ...      Make a file visible in Finder
-		 hidefile PATH ...      Hide a file in Finder
-		 restart                Restart Finder
-		 fullpathview [on/off]  Show the full path in the title of Finder windows
-		 showhidden [on/off]    Toggle visibility of hidden files and folders
+		--usage) cat <<-EOF
+		showfile PATH ...      Make a file visible in Finder
+		hidefile PATH ...      Hide a file in Finder
+		restart                Restart Finder
+		fullpathview [on/off]  Show the full path in the title of Finder windows
+		showhidden [on/off]    Toggle visibility of hidden files and folders
 		EOF
-		return 1
+		;;
+
+		showhidden) pref_bool "com.apple.finder AppleShowAllFiles" $2 && mac finder restart
+		;;
+
+		fullpathview) pref_bool "com.apple.finder _FXShowPosixPathInTitle" $2
+		;;
+
+		restart|r) osascript -e 'tell application "Finder" to quit' -e 'try' -e 'tell application "Finder" to reopen' -e 'on error' -e 'tell application "Finder" to launch' -e 'end try'
+		;;
+
+		showfile|sf)
+		shift
+		if [[ ! $finder_showfile_cmd ]]; then
+			[[ ! $(type -p setfile) && $(type -p chflags) ]] && finder_showfile_cmd="chflags nohidden" || finder_showfile_cmd="setfile -a v"
+		fi
+		for f in "$@"; do
+			[[ ! -e "$f" ]] && ERROR "file doesn't exist: $f" 1
+			$finder_showfile_cmd "$f"
+		done
+		;;
+
+		hidefile|hf)
+		shift
+		if [[ ! $finder_hidefile_cmd ]]; then
+			[[ ! $(type -p setfile) && $(type -p chflags) ]] && finder_hidefile_cmd="chflags hidden" || finder_hidefile_cmd="setfile -a V"
+		fi
+		for f in "$@"; do
+			[[ ! -e "$f" ]] && ERROR "file doesn't exist: $f" 1
+			$finder_hidefile_cmd "$f"
+		done
+		;;
+
+		*) [[ $1 ]] && ERROR "unknown command $ARG1 $ARG2"; mac help $ARG1; return 1
 		;;
 
 	esac
 	;;
 
-	i|itunes) shift
-	case $1 in
+	itunes|i) shift
+	case $lARG2 in
 
-		hideping) pref_bool "com.apple.iTunes hide-ping-dropdown" $2 ;;
-
-		storelinks) pref_bool "com.apple.iTunes show-store-link-arrows" $2 ;;
-
-		*)
-		cat <<-EOF
-		Usage: ${0##*/} itunes ...
-		  hideping [on/off]    Hide the "Ping" arrows
-		  storelinks [on/off]  Toggle display of the store link arrows
+		--usage) cat <<-EOF
+		hideping [on/off]    Hide the "Ping" arrows
+		storelinks [on/off]  Toggle display of the store link arrows
 		EOF
-		return 1
+		;;
+
+		hideping) pref_bool "com.apple.iTunes hide-ping-dropdown" $2
+		;;
+
+		storelinks) pref_bool "com.apple.iTunes show-store-link-arrows" $2
+		;;
+
+		*) [[ $1 ]] && ERROR "unknown command $ARG1 $ARG2"; mac help $ARG1; return 1
 		;;
 
 	esac
 	;;
 
-	w|wifi) shift
+	wifi|w) shift
 	local airport_path="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 	[[ ! -e "$airport_path" ]] && return 1
-	case $1 in
+	
+	case $lARG2 in
 
-		available)
-		"$airport_path" -s
-		;;
-
-		disconnect)
-		sudo "$airport_path" -z
-		;;
-
-		info)
-		"$airport_path" -i
-		;;
-
-		*)
-		cat <<-EOF
-		Usage: ${0##*/} wifi ...
-		 available   Show available wifi networks
-		 disconnect  Disassociate from any network
-		 info        Print current wireless status
+		--usage) cat <<-EOF
+		available   Show available wifi networks
+		disconnect  Disassociate from any network
+		info        Print current wireless status
 		EOF
-		return 1
+		;;
+
+		available) "$airport_path" -s
+		;;
+
+		disconnect) sudo "$airport_path" -z
+		;;
+
+		info) "$airport_path" -I
+		;;
+
+		*) [[ $1 ]] && ERROR "unknown command $ARG1 $ARG2"; mac help $ARG1; return 1
 		;;
 
 	esac
 	;;
 
-	battery)
-	ioreg -w0 -c AppleSmartBattery | grep -E '(Max|Current)Capacity' | perl -pe 's/^[\s\|]*"(\w*)Capacity" = (.*?)[\s]*$/$2 /gi' | awk '{printf "%.1f%%\n", ($2 / $1 * 100)}'
+	battery) ioreg -w0 -c AppleSmartBattery | grep -E '(Max|Current)Capacity' | perl -pe 's/^[\s\|]*"(\w*)Capacity" = (.*?)[\s]*$/$2 /gi' | awk '{printf "%.1f%%\n", ($2 / $1 * 100)}'
 	;;
 
-	flushdns)
-	dscacheutil -flushcache
+	flushdns) dscacheutil -flushcache
 	;;
 
-	group)
-	dscacheutil -q group $([[ "$2" ]] && echo "-a name $2")
+	group) dscacheutil -q group $([[ "$2" ]] && echo "-a name $2")
 	;;
 
-	lockdesktop)
-	/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend
+	lockdesktop) /System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend
 	;;
 
-	updatedb)
-	[ -e "/usr/libexec/locate.updatedb" ] && cd / && sudo /usr/libexec/locate.updatedb
+	updatedb) [ -e "/usr/libexec/locate.updatedb" ] && cd / && sudo /usr/libexec/locate.updatedb
 	;;
 
-	user)
-	dscacheutil -q user $([[ "$2" ]] && echo "-a name $2")
+	user) dscacheutil -q user $([[ "$2" ]] && echo "-a name $2")
 	;;
 
-	*)
-	[[ $1 ]] && FAIL "unknown command $1" -1
-	cat <<-EOF
-	$SCRIPT_NAME $SCRIPT_VERSION
-	Do stuff with OS X like changing settings and shit.
+	help|h)
+	[[ ! $2 ]] && echo -e "$SCRIPT_NAME $SCRIPT_VERSION\n$SCRIPT_DESC\n"
+	echo -e "Usage: ${0##*/} ${2:-<command>}${2:+ <action>}\n"
+	if [[ $2 ]]; then
+		echo "Actions:"
+		mac $2 --usage 2>/dev/null | sed 's/^/ /'
+	else
+		echo "Commands:"
+		cat <<-EOF | sed 's/^/ /'
+		$(mac dock --usage | sed 's/^/dock /')
+		
+		$(mac expose --usage | sed 's/^/expose /')
+		
+		$(mac finder --usage | sed 's/^/finder /')
+		
+		$(mac itunes --usage | sed 's/^/itunes /')
+		
+		$(mac wifi --usage | sed 's/^/wifi /')
+		
+		battery       Display battery charge (if applicable)
+		flushdns      Flush system DNS cache
+		group [NAME]  List a user (or all users) of this machine
+		help          Show this help
+		lockdesktop   Lock the desktop
+		updatedb      Update locate database
+		user [NAME]   List a user (or all users) of this machine
+		EOF
+	fi
+	;;
 
-	$(run dock help)
-
-	$(run expose help)
-
-	$(run finder help)
-
-	$(run itunes help)
-
-	$(run wifi help)
-
-	Usage: ${0##*/} ...
-	 battery       Display battery charge (if applicable)
-	 flushdns      Flush system DNS cache
-	 group [NAME]  List a user (or all users) of this machine
-	 lockdesktop   Lock the desktop
-	 updatedb      Update locate database
-	 user [NAME]   List a user (or all users) of this machine
-	EOF
-	exit 0
+	*) [[ $1 ]] && ERROR "unknown command $ARG1"; mac help; return 1
 	;;
 
 	esac
 }
 
-run "$@"
+mac "$@"
--- lipsum.php
+++ lipsum.php
@@ -25,98 +25,12 @@
  * @copyright 2012 Scott Buchanan
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link      http://wafflesnatcha.github.com
- * @version   0.1.0 2012-05-02
+ * @version   0.1.1 2012-05-04
  */
-abstract class Lipsum
-{
-
-	static $common = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-	static $list = 'lipsum';
-
-	static $lists = array(
-		'lipsum' => 'a ac accumsan ad adipiscing aenean aliquam aliquet amet ante aptent arcu at auctor augue bibendum blandit class commodo condimentum congue consectetur consequat conubia convallis cras cubilia cum curabitur curae cursus dapibus diam dictum dictumst dignissim dis dolor donec dui duis egestas eget eleifend elementum elit enim erat eros est et etiam eu euismod facilisi facilisis fames faucibus felis fermentum feugiat fringilla fusce gravida habitant habitasse hac hendrerit himenaeos iaculis id imperdiet in inceptos integer interdum ipsum justo lacinia lacus laoreet lectus leo libero ligula litora lobortis lorem luctus maecenas magna magnis malesuada massa mattis mauris metus mi molestie mollis montes morbi mus nam nascetur natoque nec neque netus nibh nisi nisl non nostra nulla nullam nunc odio orci ornare parturient pellentesque penatibus per pharetra phasellus placerat platea porta porttitor posuere potenti praesent pretium primis proin pulvinar purus quam quis quisque rhoncus ridiculus risus rutrum sagittis sapien scelerisque sed sem semper senectus sit sociis sociosqu sodales sollicitudin suscipit suspendisse taciti tellus tempor tempus tincidunt torquent tortor tristique turpis ullamcorper ultrices ultricies urna ut varius vehicula vel velit venenatis vestibulum vitae vivamus viverra volutpat vulputate',
-		'cosby' => 'bada badum bip bloo bop cachoo caw coo derp dip dum hip ka loo meep mim moom na naw nerp nup pa papa spee squee squoo woobly yee zap zip zoobity zoop zop',
-	);
-
-	static $paragraph_separator = "\n\n";
-
-	public static function setList($list)
-	{
-		$list = strtolower($list);
-		if (array_key_exists($list, self::$lists)) {
-			self::$list = $list;
-			return true;
-		} else 
-			return false;
-	}
-
-	public static function getWords()
-	{
-		$l = &self::$lists[self::$list];
-		if (!is_array($l)) 
-			$l = explode(" ", $l);
-		return $l;
-	}
-
-	/**
-	 * Returns a randomly generated sentence of lorem ipsum text.
-	 *
-	 * The first word is capitalized, and the sentence ends in either a period.
-	 * Commas are added at random.
-	 */
-	public static function sentence()
-	{
-		$words = self::getWords();
-		$sentence = array();
-		for ($section_count = mt_rand(1, 4); $section_count; --$section_count) {
-			$section = array();
-			foreach (array_rand($words, mt_rand(3, 12)) as $key) 
-				$section[] = $words[$key];
-			$sentence[] = implode($section, ' ');
-		}
-
-		# Convert to sentence case and add end punctuation.
-		return ucfirst(implode($sentence, ', ')) . ".";
-	}
-
-	/**
-	 * Returns a randomly generated paragraph of lorem ipsum text.
-	 * The paragraph consists of between 1 and 4 sentences, inclusive.
-	 */
-	public static function paragraphs($count = 1, $sentences = null)
-	{
-		
-		$paragraphs = array();
-		for ($i = 0; $i < $count; $i++) {
-			$p = array();
-			$sentences = ($sentences) ? $sentences : mt_rand(3, 5);
-			for ($i = 0; $i < $sentences; $i++) {
-				$p[] = self::sentence();
-			}
-			$paragraphs[] = implode($p, ' ');
-		}
-		return implode($paragraphs, self::$paragraph_separator);
-	}
-
-	/**
-	 * Returns a string of `count` lorem ipsum words separated by a single space.
-	 */
-	public static function words($count)
-	{
-		$words = self::getWords();
-		$output = array();
-		for ($x = 0; $x < $count; $x++) {
-			$output[] = $words[array_rand($words)];
-		}
-		return implode($output, ' ');
-	}
-}
-
 
 $script = (object) array(
 	'name' => 'lipsum.php',
-	'version' => '0.1.0 2012-05-02',
+	'version' => '0.1.1 2012-05-04',
 	'description' => 'Generate "dummy" text.',
 	'usage' => basename($_SERVER['argv'][0]) . ' [options]',
 	'options' => array(
@@ -160,7 +74,8 @@ $script = (object) array(
 			'short' => 'l:',
 			'long' => 'list:',
 			'usage' => '-l, --list=WORDLIST',
-			'description' => 'Available words lists: ' . implode(', ', array_keys(Lipsum::$lists)),
+			'description' => 'Available words lists: ' . implode(', ',
+			array_keys(Lipsum::$lists)),
 		),
 		'help' => array(
 			'short' => 'h',
@@ -169,7 +84,13 @@ $script = (object) array(
 			'description' => 'Show this help',
 		),
 	),
-	'error_handler' => function($errno, $errstr, $errfile, $errline) {
+	'error_handler' =>
+
+	function($errno,
+	$errstr,
+	$errfile,
+	$errline)
+	{
 		$message = basename($errfile) . ": " . trim($errstr);
 		error_log($message);
 		exit($code);
@@ -177,21 +98,105 @@ $script = (object) array(
 );
 set_error_handler($script->error_handler, E_USER_NOTICE);
 
-function usage()
+abstract class Lipsum
+{
+	static $common = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+	static $list = 'lipsum';
+
+	static $lists = array(
+		'lipsum' => 'a ac accumsan ad adipiscing aenean aliquam aliquet amet ante aptent arcu at auctor augue bibendum blandit class commodo condimentum congue consectetur consequat conubia convallis cras cubilia cum curabitur curae cursus dapibus diam dictum dictumst dignissim dis dolor donec dui duis egestas eget eleifend elementum elit enim erat eros est et etiam eu euismod facilisi facilisis fames faucibus felis fermentum feugiat fringilla fusce gravida habitant habitasse hac hendrerit himenaeos iaculis id imperdiet in inceptos integer interdum ipsum justo lacinia lacus laoreet lectus leo libero ligula litora lobortis lorem luctus maecenas magna magnis malesuada massa mattis mauris metus mi molestie mollis montes morbi mus nam nascetur natoque nec neque netus nibh nisi nisl non nostra nulla nullam nunc odio orci ornare parturient pellentesque penatibus per pharetra phasellus placerat platea porta porttitor posuere potenti praesent pretium primis proin pulvinar purus quam quis quisque rhoncus ridiculus risus rutrum sagittis sapien scelerisque sed sem semper senectus sit sociis sociosqu sodales sollicitudin suscipit suspendisse taciti tellus tempor tempus tincidunt torquent tortor tristique turpis ullamcorper ultrices ultricies urna ut varius vehicula vel velit venenatis vestibulum vitae vivamus viverra volutpat vulputate',
+		'cosby' => 'babity bada badum bibity bip bloo bop cachoo caw coo derp dip dum flibbity hip ka loo meep mim moom na naw nerp nup pa papa spee squee squoo voom woobly yee zam zap zim zip zoobity zoop zop',
+	);
+
+	static $paragraph_separator = "\n\n";
+
+	public static function setList($list)
+	{
+		$list = strtolower($list);
+		if (array_key_exists($list, self::$lists)) {
+			self::$list = $list;
+			return true;
+		} else
+			return false;
+	}
+
+	public static function getWords()
+	{
+		$l = &self::$lists[self::$list];
+		if (!is_array($l))
+			$l = explode(" ", $l);
+		return $l;
+	}
+
+	/**
+	 * Returns a randomly generated sentence of lorem ipsum text.
+	 *
+	 * The first word is capitalized, and the sentence ends in either a period.
+	 * Commas are added at random.
+	 */
+	public static function sentence()
+	{
+		$words = self::getWords();
+		$sentence = array();
+		for ($section_count = mt_rand(1, 4); $section_count; --$section_count) {
+			$section = array();
+			foreach (array_rand($words, mt_rand(3, 12)) as $key)
+				$section[] = $words[$key];
+			$sentence[] = implode($section, ' ');
+		}
+
+		# Convert to sentence case and add end punctuation.
+		return ucfirst(implode($sentence, ', ')) . ".";
+	}
+
+	/**
+	 * Returns a randomly generated paragraph of lorem ipsum text.
+	 * The paragraph consists of between 1 and 4 sentences, inclusive.
+	 */
+	public static function paragraphs($count = 1, $sentences = null)
+	{
+		$paragraphs = array();
+		for ($x = 0; $x < $count; $x++) {
+			$p = array();
+			$sentences = ($sentences) ? $sentences : mt_rand(3, 5);
+			for ($i = 0; $i < $sentences; $i++) {
+				$p[] = self::sentence();
+			}
+			$paragraphs[] = implode($p, ' ');
+		}
+		return implode($paragraphs, self::$paragraph_separator);
+	}
+
+	/**
+	 * Returns a string of `count` lorem ipsum words separated by a single space.
+	 */
+	public static function words($count)
+	{
+		$words = self::getWords();
+		$output = array();
+		for ($x = 0; $x < $count; $x++) {
+			$output[] = $words[array_rand($words)];
+		}
+		return implode($output, ' ');
+	}
+}
+
+function usage($s)
 {
 	echo $s->name . " " . $s->version . "\n" . ($s->description ? $s->description . "\n" : "");
-	if ($s->usage) 
+	if ($s->usage)
 		echo "\nUsage: " . $s->usage . "\n";
 	if ($s->options) {
 		echo "\nOptions:\n";
 		$lines = array();
 		$longest = 0;
-		foreach ($defs as $def_name => $def_array) {
+		foreach ($s->options as $def_name => $def_array) {
 			$lines[] = array(
 				$def_array['usage'],
 				$def_array['description'],
 			);
-			if (strlen($def_array['usage']) > $longest) 
+			if (strlen($def_array['usage']) > $longest)
 				$longest = strlen($def_array['usage']);
 		}
 		$longest = ($longest > 0) ? $longest + 2 : 0;
@@ -209,57 +214,54 @@ function usage()
 function parseOptions($s)
 {
 	$defs = $s->options;
-	
 	$short_opts = "";
 	$long_opts = array();
-	
+
 	foreach ($defs as $k => $v) {
-		if (isset($v['short'])) 
+		if (isset($v['short']))
 			$short_opts .= $v['short'];
-		if (isset($v['long'])) 
+		if (isset($v['long']))
 			$long_opts[] = $v['long'];
 	}
-	
+
 	$options = getopt($short_opts, $long_opts);
 	$args = array();
 	foreach ($options as $opt_name => $opt_value) {
 		foreach ($defs as $def_name => $def_array) {
 			if ($opt_name == rtrim($def_array['short'], ':') || $opt_name == rtrim($def_array['long'], ':')) {
-				if ($def_array['filter'] && !$args[$def_name] = filter_var($opt_value, $def_array['filter'], $def_array['filter_options'])) 
+				if ($def_array['filter'] && !$args[$def_name] = filter_var($opt_value, $def_array['filter'], $def_array['filter_options']))
 					trigger_error("invalid value for option '$opt_name'");
-				else 
+				else
 					$args[$def_name] = $opt_value;
 				continue 2;
 			}
 		}
 	}
-	
-	// Usage
+
+	// Show usage
 	if (isset($args['help'])) {
 		usage($s);
 		exit;
 	}
-	
+
 	return $args;
 }
 
 $args = parseOptions($script);
-
 if (isset($args['list'])) {
-	if (!Lipsum::setList($args['list'])) 
+	if (!Lipsum::setList($args['list']))
 		trigger_error('invalid word list');
-	
+
 	// User specified a list but no paragraphs, sentences, or words
 	// Assume they meant --paragraphs 1
-	if (!array_intersect(array('paragraphs', 'sentences', 'words'), $args)) 
+	if (!array_intersect(array('paragraphs', 'sentences', 'words'), array_keys($args)))
 		$args['paragraphs'] = 1;
 }
-
-if (isset($args['sentences'])) 
-	echo Lipsum::paragraphs(1,(int) $args['sentences']) . "\n";
-elseif (isset($args['words'])) 
+if (isset($args['paragraphs']))
+	echo Lipsum::paragraphs($args['paragraphs']) . "\n";
+elseif (isset($args['words']))
 	echo Lipsum::words((int) $args['words']);
-elseif (isset($args['paragraphs'])) 
-	echo Lipsum::paragraphs((int) $args['paragraphs']) . "\n";
-else 
+elseif (isset($args['sentences']))
+	echo Lipsum::paragraphs(1,(int) $args['sentences']) . "\n";
+else
 	echo Lipsum::$common . "\n";
