4726723f626658f3540f70546d2c0759b7f2236a
Scott Buchanan <buchanan.sc@gmail.com>
2012-06-27 19:55:17 -0700

    no message

--- Darwin/textmate.sh
+++ Darwin/textmate.sh
@@ -50,39 +50,34 @@ textmate_goto() { textmate_open "${TM_FILEPATH}" $1 $2; }
 # HTML functions
 #
 
-# HTML encode text (specifically: <, >, &) and remove blank lines
+# HTML encode text <, >, &
 #
-# Example:
+# Examples:
 # $ html_encode "<some text> you want to encode & stuff"
-#
-# Example:
 # $ cat "/some/file.html" | html_encode
 html_encode() {
 	echo -e "${@:-$(function_stdin)}" |
-		perl -pe '$| = 1; s/^[\s]*$|[ \t]*$//g; s/&/&amp;/g; s/</&lt;/g; s/>/&gt;/g;'
+		perl -pe '$|=1; s/&/&amp;/g; s/</&lt;/g; s/>/&gt;/g;'
 }
 
-# same as html_encode, but also turns newlines into <br>
-html_encode_pre() {
-	{ [ -z "$1" ] && function_stdin || echo -e "$@"; } |
-		perl -pe '$| = 1; s/&/&amp;/g; s/</&lt;/g; s/>/&gt;/g; s/$\\n/<br>/g;'
+html_encode_br() {
+	html_encode "${@:-$(function_stdin)}" | perl -pe 's/\n/<br>/g;'
 }
 
 # Open a nicely formatted HTML error message
 #
-# Example:
+# Examples:
 # $ html_error "text"
-#
-# Example:
 # $ cat some/file.txt | html_error
 html_error() {
-	[[ $TM_FILEPATH ]] && url_param="url=file:\/\/${TM_FILEPATH//\//\\/}\&"
+	# [[ $TM_FILEPATH ]] && url_param="url=file:\/\/${TM_FILEPATH//\//\\/}\&"
+	[[ $TM_FILEPATH ]] && url_param="$(regex_escape "url=file://${TM_FILEPATH}&")"
 	. "$TM_SUPPORT_PATH/lib/webpreview.sh"
-	html_header "${2:-ERROR}"
-	echo '<pre style="border:2px solid #f00;">'
+	html_header "${2:-Error}"
+	echo '<pre class="viewport error" style="border:2px solid #f00;"><code>'
 	html_encode "$@" |
 		perl -pe 's/(^.*?)((?:line )?(\d+)(?: column |\:)?(\d+))(.*$)/$1<a href=\"txmt:\/\/open\/\?'${url_param}'line=$3\&column=$4\">$2<\/a>$5/mi'
-	echo '</pre>'
+	echo '</code></pre>'
 	html_footer
 	exit_show_html
 }
@@ -93,22 +88,20 @@ html_error() {
 
 # Standard tooltip
 tooltip() {
-	tooltip_html default --text "$(html_encode_pre "$@")"
+	tooltip_html default --text "$(html_encode_br "$@")"
 }
 
 # Red tooltip with a ✘, used for a command has failed.
 tooltip_error() {
-	local input=$(html_encode_pre "$@")
+	local input=$(html_encode_br "$@")
 	tooltip_html \
 		$([[ $input ]] && echo "styled" || echo "styled_notext") \
-		--color 170,14,14 \
-		--glyph "&#x2718;" \
-		--text "$input"
+		--color 170,14,14 --glyph "&#x2718;" --text "$input"
 }
 
 # Green tooltip with a ✔, used for a command has successfully completed.
 tooltip_success() {
-	local input=$(html_encode_pre "$@")
+	local input=$(html_encode_br "$@")
 	tooltip_html \
 		$([[ $input ]] && echo "styled" || echo "styled_notext") \
 		--color 57,154,21 \
@@ -118,7 +111,7 @@ tooltip_success() {
 
 # Orange tooltip with a ⚠, used for warnings and such.
 tooltip_warning() {
-	local input=$(html_encode_pre "$@")
+	local input=$(html_encode_br "$@")
 	tooltip_html \
 		$([[ $input ]] && echo "styled" || echo "styled_notext") \
 		--color 175,82,0 \
@@ -126,31 +119,37 @@ tooltip_warning() {
 		--text "$input"
 }
 
+# tooltip_html TEMPLATE [--VAR REPLACEMENT]...
 # Show a custom tooltip using $TM_tooltip_template
 #
-# If your custom template has any %words% in it, simply pass them to this
+# If your custom template has any %%words%% in it, simply pass them to this
 # function as long arguments (i.e. tooltip_html --color 12,139,245).
 # See the included templates for more information.
 #
 # Example:
-# $ tooltip_html default --color "12,139,245" --someothervar "this is the value"
+# $ tooltip_html default --text "This is the tooltip text."
+# $ tooltip_html default --color "12,139,245" --text "This is the tooltip text."
+# $ tooltip_html default --color "12,139,245" --text "This is the tooltip text."
 tooltip_html() {
 	local template="TM_tooltip_template_$1"
 	local replacement=
 	local lookup=
 	local html="$(echo "${!template}")"
+	echo "$html"
 	shift
 
+	# Replace %%words%%
 	while (($#)); do
 		[[ ! "$1" =~ ^-- ]] && break
 		lookup="${1:2}"
 		replacement=$(regex_escape "$2")
-		html=$(echo "$html" | perl -pe "s/%${lookup}%/${replacement}/g")
+		html=$(echo "$html" | perl -pe "s/%%${lookup}%([^%]*)%/${replacement}/g")
 		shift 2
 	done
 
-	replacement=$(regex_escape "${@:-$(function_stdin)}")
-	html=$(echo "$html" | perl -pe "s/%text%/$replacement/g")
+	# Replace %%words%% that weren't specified (with their default values if possible)
+	html=$(echo "$html" | perl -pe "s/%%([a-z0-9\-\_]+)%([^%]*)%/\$2/gi")
+	
 	"${DIALOG}" tooltip --transparent --html "$html" &>/dev/null &
 }
 
@@ -167,41 +166,44 @@ exit_tooltip_warning() { tooltip_warning "$@" && exit_discard; }
 # Tooltip Templates
 #
 
-# Variables: text
+# Variables: text, [color]
 TM_tooltip_template_default=$(cat <<'EOF'
 <style>
 html,body{background:0;border:0;margin:0;padding:0}
 body{padding:1px 10px 14px}
 pre,code,tt{font-family:Menlo,monaco,monospace;font-size:inherit;margin:0}
-.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(255,255,185,.95);color:#000;font:small-caption;opacity:0;padding:2px 3px 3px;position:relative}
+.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(%%color%255,255,185%,.95);color:#000;font:small-caption;opacity:0;padding:2px 3px 3px;position:relative}
 @-webkit-keyframes fadeIn {	0% { opacity: 0 } 100% { opacity: .9999 } }
 </style>
-<div class="tooltip">%text%</div>
+<div class="tooltip">%%text%%</div>
 EOF)
 
-# Variables: color, glyph, text
+# Variables: glyph, text, [color]
 TM_tooltip_template_styled=$(cat <<'EOF'
 <style>
 html,body{background:0;border:0;margin:0;padding:0}
+
+
+
 body{padding:1px 10px 14px}
 pre,code,tt{font-family:Menlo,monaco,monospace;font-size:inherit;margin:0}
-.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-border-radius:2px 0 0 2px;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(%color%,.95);color:#fff;font:small-caption;opacity:0;position:relative;text-shadow:0 1px 0 rgba(0,0,0,.2)}
+.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-border-radius:2px 0 0 2px;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(%%color%255,255,185%,.95);color:#fff;font:small-caption;opacity:0;position:relative;text-shadow:0 1px 0 rgba(0,0,0,.2)}
 .glyph{-webkit-border-radius:2px 0 0 2px;-webkit-box-shadow:-8px 0 8px -8px rgba(0,0,0,.3) inset;-webkit-box-sizing:border-box;-webkit-mask-image:-webkit-linear-gradient(top,rgba(0,0,0,1) 75%,rgba(0,0,0,.5));background-image:-webkit-linear-gradient(top,rgba(0,0,0,.2),rgba(0,0,0,.1));box-sizing:border-box;font-family:monospace,webdings;height:100%;padding:2px 0 0;position:absolute;text-align:center;text-shadow:0 -1px 0 rgba(0,0,0,.2);width:18px}
-.text{padding:2px 3px 3px 4px;margin-left:18px}
+.text{margin-left:18px;padding:2px 3px 3px 4px}
 @-webkit-keyframes fadeIn {	0% { opacity: 0 } 100% { opacity: .9999 } }
 </style>
-<div class="tooltip"><div class="glyph">%glyph%</div><div class="text">%text%</div></div>
+<div class="tooltip"><div class="glyph">%%glyph%%</div><div class="text">%%text%%</div></div>
 EOF)
 
-# Variables: color, glyph
+# Variables: glyph, [color]
 TM_tooltip_template_styled_notext=$(cat <<'EOF'
 <style>
 html,body{background:0;border:0;margin:0;padding:0}
 body{padding:1px 10px 14px}
-.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-border-radius:5px;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(%color%,.95);color:#fff;font:16px/25px monospace,webdings;height:25px;opacity:0;padding:3px;position:relative;text-align:center;text-shadow:0 1px 0 rgba(0,0,0,.2);width:25px}
+.tooltip{-webkit-animation:fadeIn .2s ease 0s;-webkit-animation-fill-mode:forwards;-webkit-border-radius:5px;-webkit-box-shadow:0 0 0 1px rgba(0,0,0,.1),0 5px 9px 0 rgba(0,0,0,.4);background:rgba(%%color%255,255,185%,.95);color:#fff;font:16px/25px monospace,webdings;height:25px;opacity:0;padding:3px;position:relative;text-align:center;text-shadow:0 1px 0 rgba(0,0,0,.2);width:25px}
 @-webkit-keyframes fadeIn {	0% { opacity: 0 } 100% { opacity: .9999 } }
 </style>
-<div class="tooltip">%glyph%</div>
+<div class="tooltip">%%glyph%%</div>
 EOF)
 
 
