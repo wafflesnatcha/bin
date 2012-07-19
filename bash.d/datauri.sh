4585080d9673db3d999fb89ec20cdbd310241633
Scott Buchanan <buchanan.sc@gmail.com>
2012-07-18 23:03:18 -0700

    no message

---
+++ bash.d/datauri.sh
@@ -0,0 +1,9 @@
+# datauri FILE
+
+datauri() {
+	[ -z "$1" ] && return
+	echo -n "data:$(file -b --mime-type "$1");base64," &&
+		openssl base64 -in "$1" |
+		awk '{ str1=str1 $0 }END{ print str1 }' |
+		perl -pe 's/\s+$//'
+}
---
+++ bash.d/string_repeat.sh
@@ -0,0 +1,7 @@
+# string_repeat STRING [MULTIPLIER]
+# Output a string multiple times.
+#
+# Example (courtesy of Dave Grohl):
+# $ string_repeat "THE BEST " 7
+# THE BEST THE BEST THE BEST THE BEST THE BEST THE BEST THE BEST
+string_repeat() { for (( c=1; c<=${2:-1}; c++)); do printf "$1"; done; }
