d9a3bf974622de281ce3172894c6a2a5a95e9645
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-07 05:10:27 -0700

    cleaning up, prettier output

--- Darwin/update_tmbundles.sh
+++ Darwin/update_tmbundles.sh
@@ -1,51 +1,47 @@
 #!/usr/bin/env bash
-source colors.sh 2>/dev/null
+. colors.sh 2>/dev/null
+
+clear_line() { [[ ! $COLOR_SUPPORTED ]] && return 1; [[ ! $cols ]] && cols=${COLUMNS:-$(tput cols)}; printf "\r%${cols}s\r" ""; [[ $1 ]] && echo -en "$*"; }
 
 git_update() {
 	for d in "$@"; do
 		[[ ! -d "$d/.git" ]] && continue
 		local name=$(basename "$d")
-		echo -en "${COLOR_YELLOW}$name...${COLOR_RESET} "
-		
+		echo -en "${COLOR_RESET}  $name...${COLOR_RESET} "
 		local res="$({ cd "$d" 1>/dev/null && git pull; } 2>&1)"
 		local code=$?
 		
-		if [[ $code != 0 ]]; then
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_RED}$name... ${COLOR_RESET}"
-			echo -e "\n$res" >&2
+		if [[ $code -gt 0 ]]; then
+			clear_line "${COLOR_RED}✘ $name...${COLOR_RESET}\n" || echo "ERROR"
+			echo "$res" >&2
 			return $code
 		elif [[ "$res" = "Already up-to-date." ]]; then
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_WHITE}$name... ${COLOR_RESET} "
-			echo -e "$res"
+			clear_line "${COLOR_WHITE}✔ $name...${COLOR_RESET}\n" || echo "$res"
 		else
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_GREEN}$name... ${COLOR_RESET}\n"
-			echo -e "$res" | sed 's/^/  /'
+			clear_line "${COLOR_GREEN}✔ $name...${COLOR_RESET}\n"
+			echo "$res" | sed 's/^/  /'
 		fi
 	done
 }
 
 svn_update() {	
 	for d in "$@"; do
-		[[ ! -d "$d/.svn" ]] && continue
-		
-		local name=$(basename "$d")
+		[[ ! -d "$d/.svn" ]] && continue		
 		local rev=$(svn info "$d" | grep -i "Revision:" | sed 's/Revision: //')
-		
-		echo -en "${COLOR_BLUE}$name ...${COLOR_RESET} ($rev) "
-		
+		local name="$(basename "$d") (r$rev)"
+		echo -en "${COLOR_RESET}  $name...${COLOR_RESET} "
 		local res=$(svn update "$d")
 		local code=$?
 
-		if [[ $? != 0 ]]; then
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_RED}$name ...${COLOR_RESET} ($rev) "
-			echo -e "\n$res" >&2
+		if [[ $code -gt 0 ]]; then
+			clear_line "${COLOR_RED}✘ $name...${COLOR_RESET}\n" || echo "ERROR"
+			echo "$res" >&2
 			return $code
 		elif [[ $res =~ ^At\ revision ]]; then
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_WHITE}$name... ${COLOR_RESET} "
-			echo -e "$res"
+			clear_line "${COLOR_WHITE}✔ $name...${COLOR_RESET}\n" || echo "$res"
 		else
-			[[ $COLOR_SUPPORTED ]] && echo -en "\r${COLOR_GREEN}$name... ${COLOR_RESET}\n"
-			echo -e "$res" | sed 's/^/  /'
+			clear_line "${COLOR_GREEN}✔ $name...${COLOR_RESET}\n"
+			echo "$res"
 		fi
 	done
 }
