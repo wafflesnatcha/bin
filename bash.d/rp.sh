e33c191ccb9654895fd699c24b74d7d5a34555f8
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-06 20:39:43 -0700

    Adding limited support for shell aliases New command `cdrp`:  `cd` using the path from `rp`

--- bash.d/rp.sh
+++ bash.d/rp.sh
@@ -4,7 +4,18 @@
 # FILE can be either a path to an existing file, or any file that exists in the
 # system $PATH.
 rp() {
-	local FILE="$1"
-	[[ ! -e "$FILE" && $(type -p "$FILE") ]] && FILE="$(type -p "$FILE")"
+	local FILE="$*"
+	[[ ! -e "$FILE" && "$(type -t "$FILE")" = "alias" ]] && FILE="$(alias "$FILE" | perl -pe 's/^alias .*='\''(.*?)(?: \-{1,2}.+)?'\''$/$1/')"
+	[[ ! -e "$FILE" ]] && FILE="$(which "$FILE" 2>/dev/null)"
 	readlink -f "$FILE" 2>/dev/null || type -p greadlink &>/dev/null && greadlink -f "$FILE"
+}
+
+# cdrp FILE
+# Change directory using path from `rp` command
+cdrp() {
+	local p="$(rp "$*")"
+	[[ "$p" && ! -d "$p" ]] && p="$(dirname "$p")"
+	[[ ! "$p" || ! -d "$p" ]] && return 1
+	echo "$p" 1>&2
+	cd "$p"
 }
