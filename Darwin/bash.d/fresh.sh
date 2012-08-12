db8080a7f6e86693ce2f86d2c167e433ec0dea92
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-11 19:27:54 -0700

    when using `fresh()`, random entries may appear at the end of the history of the new shell; this was due to the previous tab closing (and writing it's old history) before the new tab's shell had actually loaded

--- Darwin/bash.d/fresh.sh
+++ Darwin/bash.d/fresh.sh
@@ -1,18 +1,24 @@
+# fresh
 # Save the command history and open a new tab at the current directory
 fresh() {
-	history -w && osascript <<-EOF
+	history -w
+	osascript <<-EOF
 	tell application "System Events"
 		if not UI elements enabled then return
 		tell application process "Terminal"
 			if not frontmost then return 1
-			--click menu item "New Tab" of menu "Shell" of menu bar 1
 			keystroke "t" using command down
-			tell application "Terminal" to do script "cd '$PWD' && history -c && history -r" in front window
 		end tell
 	end tell
+	tell application "Terminal" to repeat with w in windows
+		try
+			if frontmost of w is true then do script " cd '$PWD'" in w
+		end try
+	end repeat
 	return
 	EOF
 }
 
+# freshe
 # Run `fresh` and exit
-freshe() { fresh && exit; }
+# freshe() { fresh && exit; }
