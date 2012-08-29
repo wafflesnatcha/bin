43c45c837a8f963f3ed03d42c40a0bf8434e7d5a
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-28 19:23:45 -0700

    `goo.gl URL` # Shorten a URL using the Google URL Shortener service (http://goo.gl).

---
+++ bash.d/goo.gl.sh
@@ -0,0 +1,9 @@
+# goo.gl URL
+# Shorten a URL using the Google URL Shortener service (http://goo.gl).
+goo.gl() {
+	curl -qsSL -m10 --connect-timeout 10 \
+		https://www.googleapis.com/urlshortener/v1/url \
+		-H 'Content-Type: application/json' \
+		-d '{"longUrl": "'${1//\"/\\\"}'"}' |
+		perl -ne 'if(m/^\s*"id":\s*"(.*)",?$/i) { print $1 }'
+}
