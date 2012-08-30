074b07e370d3bdeb876c2c7717f39b672e104b33
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-30 01:05:12 -0700

    ignoring coding standards warnings... bleh

--- lipsum.php
+++ lipsum.php
@@ -8,29 +8,38 @@
  * @author    Scott Buchanan <buchanan.sc@gmail.com>
  * @copyright 2012 Scott Buchanan
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
- * @version   r2 2012-07-06
+ * @version   r3 2012-08-29
  * @link      http://wafflesnatcha.github.com
  */
-require_once ("CLIScript.php");
+error_reporting(E_ALL & ~E_NOTICE);
+require_once ('CLIScript.php');
+
 /**
  * Lipsum generator
  */
 abstract class Lipsum
 {
 	static $_default = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	static $_list = 'lipsum';
 	static $_lists = array(
 		'lipsum' => 'a ac accumsan ad adipiscing aenean aliquam aliquet amet ante aptent arcu at auctor augue bibendum blandit class commodo condimentum congue consectetur consequat conubia convallis cras cubilia cum curabitur curae cursus dapibus diam dictum dictumst dignissim dis dolor donec dui duis egestas eget eleifend elementum elit enim erat eros est et etiam eu euismod facilisi facilisis fames faucibus felis fermentum feugiat fringilla fusce gravida habitant habitasse hac hendrerit himenaeos iaculis id imperdiet in inceptos integer interdum ipsum justo lacinia lacus laoreet lectus leo libero ligula litora lobortis lorem luctus maecenas magna magnis malesuada massa mattis mauris metus mi molestie mollis montes morbi mus nam nascetur natoque nec neque netus nibh nisi nisl non nostra nulla nullam nunc odio orci ornare parturient pellentesque penatibus per pharetra phasellus placerat platea porta porttitor posuere potenti praesent pretium primis proin pulvinar purus quam quis quisque rhoncus ridiculus risus rutrum sagittis sapien scelerisque sed sem semper senectus sit sociis sociosqu sodales sollicitudin suscipit suspendisse taciti tellus tempor tempus tincidunt torquent tortor tristique turpis ullamcorper ultrices ultricies urna ut varius vehicula vel velit venenatis vestibulum vitae vivamus viverra volutpat vulputate',
 		'cosby' => 'babity bada badum bibity bip bloo bop cachoo caw coo derp dip dum flibbity hip ka loo meep mim moom na naw nerp nup pa papa spee squee squoo voom woobly yee zam zap zim zip zoobity zoop zop',
 	);
+	static $_list = 'lipsum';
 	static $_separator = array(
 		'paragraph' => "\n\n",
 		'sentence' => ' ',
 		'word' => ' '
 	);
+	
+	/**
+	 * Get/set the activate word list
+	 *
+	 * @param string $list Leave empty to just retreive the word list.
+	 * @return array|boolean Returns an array of words, or `false` if $list is invalid.
+	 */
 	static function wordList($list = null)
 	{
-		if (isset($list)) {
+		if ($list) {
 			$list = strtolower($list);
 			if (array_key_exists($list, self::$_lists)) self::$_list = $list;
 			else return false;
@@ -39,6 +48,7 @@ abstract class Lipsum
 		if (!is_array($l)) $l = explode(" ", $l);
 		return $l;
 	}
+
 	/**
 	 * Returns a randomly generated sentence.
 	 *
@@ -72,6 +82,7 @@ abstract class Lipsum
 		}
 		return implode($paragraphs, self::$_separator['paragraph']);
 	}
+
 	/**
 	 * Returns a string of `count` lorem ipsum words separated by a single space.
 	 */
@@ -85,6 +96,7 @@ abstract class Lipsum
 		return implode($output, self::$_separator['word']);
 	}
 }
+
 $script = new CLIScript(array(
 	'name' => 'lipsum.php',
 	'description' => 'Generate "dummy" text.',
@@ -94,61 +106,54 @@ $script = new CLIScript(array(
 		'paragraphs' => array(
 			'short' => 'p:',
 			'long' => 'paragraphs:',
-			'filter' => FILTER_VALIDATE_INT,
-			'filter_options' => array(
-				'options' => array(
-					'min_range' => 1,
-				),
-			),
 			'usage' => '-p, --paragraphs NUM',
 			'description' => 'Output text in paragraphs',
+			'filter' => FILTER_VALIDATE_INT,
+			'filter_options' => array('options' => array('min_range' => 1)),
 		),
 		'sentences' => array(
 			'short' => 's:',
 			'long' => 'sentences:',
-			'filter' => FILTER_VALIDATE_INT,
-			'filter_options' => array(
-				'options' => array(
-					'min_range' => 1,
-				),
-			),
 			'usage' => '-s, --sentences NUM',
 			'description' => 'Output text in sentences, or when used in conjunction with -p, will define sentences per paragraph.',
+			'filter' => FILTER_VALIDATE_INT,
+			'filter_options' => array('options' => array('min_range' => 1)),
 		),
 		'words' => array(
 			'short' => 'w:',
 			'long' => 'words:',
-			'filter' => FILTER_VALIDATE_INT,
-			'filter_options' => array(
-				'options' => array(
-					'min_range' => 1,
-				),
-			),
 			'usage' => '-w, --words NUM',
 			'description' => 'Output NUM words separated by a single space',
+			'filter' => FILTER_VALIDATE_INT,
+			'filter_options' => array('options' => array('min_range' => 1)),
 		),
 		'list' => array(
 			'short' => 'l:',
 			'long' => 'list:',
 			'usage' => '-l, --list WORDLIST',
-			'description' => 'Available words lists: ' . implode(', ', array_keys(Lipsum::$_lists)) ,
+			'description' => 'Available words lists: ' . str_replace(Lipsum::$_list, Lipsum::$_list . " (default)", implode(', ', array_keys(Lipsum::$_lists))),
 		)
 	)
 ));
+
 $args = $script->parseArgs();
-if (isset($args['list'])) {
-	if (!Lipsum::wordList($args['list'])) trigger_error('invalid word list');
-	// User specified a list but no paragraphs, sentences, or words
-	// Assume they meant --paragraphs 1
-	if (!array_intersect(array(
-		'paragraphs',
-		'sentences',
-		'words'
-	), array_keys($args))) $args['paragraphs'] = 1;
+$args['list'] = $args['list']? : ($_SERVER['LIPSUM_LIST']? : null);
+
+if ($args['list']) {
+	if (!Lipsum::wordList($args['list'])) {
+		trigger_error('invalid word list');
+	}
+
+	// User specified a list but no paragraphs, sentences, or words.
+	// Assume they meant `--paragraphs 1`
+	if (!array_intersect(array('paragraphs', 'sentences', 'words'), array_keys($args))) {
+		$args['paragraphs'] = 1;
+	}
 }
-if (isset($args['paragraphs']) || isset($args['sentences'])) {
-	echo Lipsum::paragraphs(isset($args['paragraphs']) ? $args['paragraphs'] : 1, isset($args['sentences']) ? $args['sentences'] : null) . "\n";
-} else if (isset($args['words'])) {
+
+if ($args['paragraphs'] || $args['sentences']) {
+	echo Lipsum::paragraphs($args['paragraphs']? $args['paragraphs'] : 1, $args['sentences']? $args['sentences'] : null) . "\n";
+} else if ($args['words']) {
 	echo Lipsum::words((int)$args['words']);
 } else {
 	echo Lipsum::$_default . "\n";
