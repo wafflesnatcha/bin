cc28f0b7bc3815855593085f0c85d9b5288136a9
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-02 15:19:16 -0700

    added -x and -l flags

--- bash.d/env_functions.sh
+++ bash.d/env_functions.sh
@@ -1,17 +1,21 @@
-# env_functions
+# env_functions [-x|-l]
+#
 # Same as `env_vars`, but for functions. Exported functions have their name and
-# brackets colored if supported.
+# brackets colored if supported. The -x option shows only exported variables,
+# while the -l option shows the opposite.
 env_functions() {
 	local c=()
-	[[ ! -p /dev/stdout && "$TERM" =~ xterm-(256)?color ]] && c=( "\033[m" "\033[1;32m" "\033[1m" )
-
-	# Use `pygmentize` if available
-	# [[ ! -p /dev/stdout && "$TERM" =~ xterm-(256)?color ]] && type -p pygmentize &>/dev/null && { declare -f | pygmentize -l bash; return; }
-
+	[[ ! -p /dev/stdout && "$TERM" =~ xterm-(256)?color ]] && c=( "\033[m" "\033[1;32m" "\033[1;33m" )
 	declare -F | sed -E 's/declare \-f((x) |( ))/f\2\3 /' | {
 		while read fn; do
-			# different color function name for exported functions
-			[[ ${fn:0:2} = "fx" ]] && c[9]=${c[1]} || c[9]=${c[2]}
+
+			if [[ ${fn:0:2} = "fx" ]]; then
+				[[ $1 = "-l" ]] && continue
+				c[9]=${c[1]}
+			else
+				[[ $1 = "-x" ]] && continue
+				c[9]=${c[2]}
+			fi
 
 			echo -e "${c[9]}${fn:3} ()\n{${c[0]}"
 			type "${fn:3}" | sed '1d;2d;3d;$d'
