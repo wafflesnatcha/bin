fe95c3219f24bcaaf03da4f9c5d897a45318fcb6
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-04 19:56:55 -0700

    added usage info

---
+++ bash.d/watch_clipboard.sh
@@ -0,0 +1,28 @@
+# watch_clipboard [INTERVAL]
+#
+# Watch the clipboard for changes and display them to stdout. INTERVAL is the
+# time (in seconds) to wait after the clipboard is tested for a change (default
+# is 0.25 seconds).
+watch_clipboard() {
+	type -a xclip pbpaste &>/dev/null ||
+		{ echo "watch_clipboard: requires either \`xclip\` or \`pbcopy\`" 1>&2; return 2; }
+
+	[[ $1 = "-h" || $1 = "--help" ]] && cat <<-EOF && return
+		Usage: watch_clipboard [INTERVAL]
+
+		Watch the clipboard for changes and display them to stdout. INTERVAL is the
+		time (in seconds) to wait after scanning the clipboard for changes (default
+		0.25 seconds).
+		EOF
+
+	local cmd current last interval=${1:-0.25}
+
+	type xclip &>/dev/null && cmd="xclip -o" || cmd="pbpaste"
+
+	while true; do
+		current="$($cmd)"
+		[[ "$current" != "$last" ]] && echo "$current"
+		last="$current"
+		sleep $interval || return 2
+	done
+}
