90e603537702798aacf49570dfa9a1b43fd94185
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-04 20:13:14 -0700

    added usage info cleaned up

--- Darwin/bash.d/summarize.sh
+++ Darwin/bash.d/summarize.sh
@@ -1,10 +1,11 @@
 # summarize SENTENCES
-# 
+#
 # Use OS X summary service (via AppleScript) to summarize stdin.
 summarize() {
-	osascript \
-	    -e "on run argv" \
-	    -e "return summarize (do shell script \"cat\") in (item 1 of argv)" \
-	    -e "end run" \
-	    $*
+	[[ ! $1 || $1 = "-h" || $1 = "--help" ]] && cat <<-EOF && return
+		Usage: summarize SENTENCES
+
+		Use the OS X summary service to summarize stdin.
+		EOF
+	osascript -e "summarize (do shell script \"cat\") in $1"
 }
