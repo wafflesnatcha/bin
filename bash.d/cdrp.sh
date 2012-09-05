d7389deeaf37338b3cb8ee6a183cfc31e38d9381
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-04 20:14:27 -0700

    moved `cdrp` to it's own file

---
+++ bash.d/cdrp.sh
@@ -0,0 +1,10 @@
+# cdrp FILE
+# 
+# Change directory to the path of the result of `rp`.
+cdrp() {
+	local p=$(rp "$*") || return
+	[[ "$p" && ! -d "$p" ]] && p="$(dirname "$p")"
+	[[ ! "$p" || ! -d "$p" ]] && return
+	echo "$p" 1>&2
+	cd "$p"
+}
--- bash.d/rp.sh
+++ bash.d/rp.sh
@@ -1,6 +1,7 @@
 # rp FILE
-# Show the end value of a symbolic link (like GNU `readlink -f`).
 # 
+# Show the end value of a symbolic link (like GNU `readlink -f`).
+#
 # FILE can be either a path to an existing file, or any file that exists in the
 # system $PATH.
 rp() {
@@ -8,14 +9,4 @@ rp() {
 	[[ ! -e "$FILE" && "$(type -t "$FILE")" = "alias" ]] && FILE="$(alias "$FILE" | perl -pe 's/^alias .*='\''(.*?)(?: \-{1,2}.+)?'\''$/$1/')"
 	[[ ! -e "$FILE" ]] && FILE="$(which "$FILE" 2>/dev/null)"
 	readlink -f "$FILE" 2>/dev/null || type -p greadlink &>/dev/null && greadlink -f "$FILE"
-}
-
-# cdrp FILE
-# Change directory to the path of the result of `rp`.
-cdrp() {
-	local p=$(rp "$*") || return
-	[[ "$p" && ! -d "$p" ]] && p="$(dirname "$p")"
-	[[ ! "$p" || ! -d "$p" ]] && return
-	echo "$p" 1>&2
-	cd "$p"
 }
