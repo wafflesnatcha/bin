01ed24c3d4ac7eef1bc4217de327d042d874e978
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-10 19:07:15 -0700

    `datauri` attempts to use the `base64` command first, and falls back to `openssl` if it's unavailable also outputs usage info if no parameters

--- bash.d/datauri.sh
+++ bash.d/datauri.sh
@@ -1,8 +1,17 @@
 # datauri FILE
+# 
+# Convert a file to a data:URI string, suitable for web content.
 datauri() {
-	[ -z "$1" ] && return
-	echo -n "data:$(file -b --mime-type "$1");base64," &&
-		openssl base64 -in "$1" |
-		awk '{ str1=str1 $0 }END{ print str1 }' |
-		perl -pe 's/\s+$//'
+	[[ ! $1 ]] && cat <<-EOF && return
+		Usage: datauri FILE
+
+		Convert a file to a data:URI string, suitable for web content.
+		EOF
+		
+	echo -n "data:$(file -b --mime-type "$1");base64,"
+	if type base64 &>/dev/null; then
+		base64 -i "$1" | perl -pe 's/\s+$//'
+	elif type openssl &>/dev/null; then
+		openssl base64 -in "$1" | awk '{ str1=str1 $0 }END{ print str1 }' | perl -pe 's/\s+$//'
+	fi
 }
