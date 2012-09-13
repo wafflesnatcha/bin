f8f7b3c24332723e4265ee7c291b644be98bffbd
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-12 19:42:59 -0700

    cleaned up

--- crush.sh
+++ crush.sh
@@ -10,43 +10,36 @@ Optimize images with either optipng, pngcrush, or jpgcrush.
 Usage: ${0##*/} [OPTION]... FILE...
 
 Options:
- -p, --percentage  Prefix output lines with overall percent completed (useful
-                   when piping to CocoaDialog progressbar)
+ -p, --percentage  Prefix output lines with overall percent completed
  -h, --help        Show this help
 EOF
 }
 
 ERROR() { [[ $1 ]] && echo "$SCRIPT_NAME: $1" 1>&2; [[ $2 > -1 ]] && exit $2; }
 
-temp_file() {
-	local var
-	for var in "$@"; do
-		eval $var=\"$(mktemp -t "${0##*/}")\"
-		temp_file__files="$temp_file__files '${!var}'"
-	done
-	trap "rm -f $temp_file__files" EXIT
-}
-
-type optipng &>/dev/null && opt_optipng="optipng"
-type pngcrush &>/dev/null && opt_pngcrush="pngcrush"
-type jpgcrush &>/dev/null && opt_jpgcrush="jpgcrush"
-
 while (($#)); do
 	case $1 in
-		-h|--help) usage; exit 0 ;;
-		-p|--percentage) opt_percentage=1 ;;
-		--) shift; break ;;
-		-*|--*) ERROR "unknown option ${1}" 1 ;;
-		*) break ;;
+		-h|--help)
+			usage; exit 0 ;;
+		-p|--percentage)
+			opt_percentage=1 ;;
+
+		--) shift; break ;; -*|--*) ERROR "unknown option ${1}" 1 ;; *) break ;;
 	esac
 	shift
 done
 
 [[ ! $1 ]] && { usage; exit 0; }
 
+type optipng &>/dev/null && opt_optipng="optipng"
+type pngcrush &>/dev/null && opt_pngcrush="pngcrush"
+type jpgcrush &>/dev/null && opt_jpgcrush="jpgcrush"
+
 process() {
-	fext=$(echo "${1##*.}" | tr '[:upper:]' '[:lower:]')
-	case "$fext" in
+	local ext size_before size_after
+	ext=$(echo "${1##*.}" | tr '[:upper:]' '[:lower:]')
+	size_before=$(stat -f%z "$1")
+	case "$ext" in
 		png)
 		[[ ! $opt_pngcrush && ! $opt_optipng ]] && return 1
 		[[ $opt_pngcrush ]] && "$opt_pngcrush" -rem gAMA -rem alla -rem text -oldtimestamp -ow "$1" 2>/dev/null
@@ -58,13 +51,15 @@ process() {
 		;;
 		*) return 1 ;;
 	esac
+	size_after=$(stat -f%z "$1")
 }
 
 count=0
 for f in "$@"; do
 	(( count++ ))
 	percent=$(echo "$count/$#*100" | bc -l | xargs printf "%1.0f%%";)
-	[[ $opt_percentage ]] && echo -n "$percent [$percent] "
-	echo "$(basename "$f")"
+	process "$f" || continue
+	[[ $opt_percentage ]] && echo -n "$percent "
+	echo "$f"
 	process "$f"
 done
