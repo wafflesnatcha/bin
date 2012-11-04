e2c1eb7badf4ee682133fe6a51efa89bf4394d15
Scott Buchanan <buchanan.sc@gmail.com>
2012-11-04 01:28:47 -0800

    remade browser.sh

--- browser.sh
+++ browser.sh
@@ -1,25 +1,42 @@
-#!/bin/sh -e
-#
-# Usage: browser
-#        pipe html to a browser
-# e.g.
-#  $ echo '<h1>hi mom!</h1>' | browser
-#  $ ron -5 man/rip.5.ron | browser   
+#!/usr/bin/env bash
+# `browser.sh` by Scott Buchanan <buchanan.sc@gmail.com> http://wafflesnatcha.github.com
+SCRIPT_NAME="browser.sh"
+SCRIPT_VERSION="r1 2012-11-03"
 
-if [ -t 0 ]; then
-  if [ -n "$1" ]; then
-    open $1
-  else
-    cat <<usage
-Usage: browser
-       pipe html to a browser
+usage() { cat <<EOF
+$SCRIPT_NAME $SCRIPT_VERSION
+Pipe HTML into the default browser.
 
-$ echo '<h1>hi mom!</h1>' | browser
-$ ron -5 man/rip.5.ron | browser
-usage
-fi
-else
-  f="/tmp/browser.$RANDOM.html"
-  cat /dev/stdin > $f
-  open $f
-fi
+Usage: ${0##*/} [OPTION]...
+
+Options:
+ -b, --body  Wrap input in a proper HTML document. Use this if the input
+             contains only the HTML body. 
+ -h, --help  Show this help
+EOF
+}
+
+ERROR() { [[ $1 ]] && echo "$SCRIPT_NAME: $1" 1>&2; [[ $2 > -1 ]] && exit $2; }
+
+while (($#)); do
+	case $1 in
+		-h|--help)
+			usage; exit 0 ;;
+		-f|--body)
+			opt_body=1 ;;
+
+		--) shift; break ;;
+		-*|--*) ERROR "unknown option ${1}" 1 ;;
+		*) break ;;
+	esac
+	shift
+done
+
+tmpfile=$(mktemp -t "${0##*/}")
+mv "$tmpfile" "$tmpfile.html"
+tmpfile="$tmpfile.html"
+
+[[ $opt_body ]] && echo "<!DOCTYPE html><html><head><title>$(basename "$tmpfile")</title></head><body>" >> "$tmpfile"
+cat >> "$tmpfile"
+[[ $opt_body ]] && echo "</body></html>" >> "$tmpfile"
+open $tmpfile
