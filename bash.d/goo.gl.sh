a4c60669ebf0ed8d5a474612756bfa97b75aa683
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-04 19:27:19 -0800

    displays usage when no parameters

--- bash.d/goo.gl.sh
+++ bash.d/goo.gl.sh
@@ -2,9 +2,10 @@
 # 
 # Shorten a URL using the Google URL Shortener service (http://goo.gl).
 goo.gl() {
+	[[ ! $1 ]] && { echo -e "Usage: goo.gl [URL]\n\nShorten a URL using the Google URL Shortener service (http://goo.gl)."; return; }
 	curl -qsSL -m10 --connect-timeout 10 \
-		https://www.googleapis.com/urlshortener/v1/url \
+		'https://www.googleapis.com/urlshortener/v1/url' \
 		-H 'Content-Type: application/json' \
-		-d '{"longUrl": "'${1//\"/\\\"}'"}' |
+		-d '{"longUrl":"'${1//\"/\\\"}'"}' |
 		perl -ne 'if(m/^\s*"id":\s*"(.*)",?$/i) { print $1 }'
 }
