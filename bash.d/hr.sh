718e4723599394fc71146c92f509fb87d690d8a8
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-04 20:11:24 -0800

    cleaned up

--- bash.d/hr.sh
+++ bash.d/hr.sh
@@ -6,13 +6,13 @@ hr() {
 	
 	# No color support
 	if [[ -p /dev/stdout || ! "$TERM" =~ xterm-(256)?color ]]; then
-		printf "%-${c}s" "" | tr " " "#"
+		printf "%-72s" "" | tr " " "#"
 		return
 	fi
 	
 	if [[ "$TERM" =~ xterm-256color ]]; then
-		# r=( $(echo "\033[38;5;"{17..21}m "\033[38;5;"{20..17}m) )
-		r=( $(echo "\033[38;5;"{125..129}m "\033[38;5;"{128..125}m) )
+		# r=( $(echo "\033[38;5;"{17..21}m "\033[38;5;"{20..17}m) )		# Blue color scheme
+		r=( $(echo "\033[38;5;"{125..129}m "\033[38;5;"{128..125}m) )	# Magenta color scheme
 	elif [[ "$TERM" =~ xterm-color ]]; then
 		r=( $(echo \033[{31..36}m \033[{36..31}m) )	
 	fi
