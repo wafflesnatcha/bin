bed72fd7837b113bd1ef628525407921d08ee0ac
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-26 07:47:03 -0800

    renaming function: `watch_clipboard` => `watch-clipboard`

--- bash.d/watch-clipboard.sh
+++ bash.d/watch-clipboard.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Usage: watch_clipboard [INTERVAL]
+# Usage: watch-clipboard [INTERVAL]
 #
 # Watch the clipboard for changes and display them to stdout. INTERVAL is the
 # time (in seconds) to wait after the clipboard is tested for a change (default
 # is 0.25 seconds).
-watch_clipboard() {
+watch-clipboard() {
 	type -a xclip pbpaste &>/dev/null ||
-		{ echo "watch_clipboard: requires either \`xclip\` or \`pbcopy\`" 1>&2; return 2; }
+		{ echo "watch-clipboard: requires either \`xclip\` or \`pbcopy\`" 1>&2; return 2; }
 
 	[[ $1 = "-h" || $1 = "--help" ]] && cat <<-EOF && return
-		Usage: watch_clipboard [INTERVAL]
+		Usage: watch-clipboard [INTERVAL]
 
 		Watch the clipboard for changes and display them to stdout. INTERVAL is the
 		time (in seconds) to wait after scanning the clipboard for changes (default
