bbe2aa50944bf53191974261ac80c3c220a0cd54
Scott Buchanan <buchanan.sc@gmail.com>
2013-01-24 12:43:35 -0800

    renaming 'findstring.sh' to 'find-string.sh', also some small cleanup

--- find-string.sh
+++ find-string.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# `findstring.sh` by Scott Buchanan <http://wafflesnatcha.github.com>
-SCRIPT_NAME="findstring.sh"
-SCRIPT_VERSION="r1 2012-07-11"
+# `find-string.sh` by Scott Buchanan <http://wafflesnatcha.github.com>
+SCRIPT_NAME="find-string.sh"
+SCRIPT_VERSION="r2 2013-01-24"
 
 usage() { cat <<EOF
 $SCRIPT_NAME $SCRIPT_VERSION
-Recursively find strings in files.
+Search files for text.
 
 Usage: ${0##*/} [OPTION]... PATTERN
 
@@ -25,19 +25,25 @@ opt_binary=
 opt_depth=
 opt_filenames=
 opt_ignore_case=
-opt_path="."
+opt_path=.
 
 while (($#)); do
 	case $1 in
-		-h|--help) usage; exit 0 ;;
-		-b|--binary) opt_binary=1 ;;
+		-h|--help)
+			usage; exit 0 ;;
+		-b|--binary)
+			opt_binary=1 ;;
 		-d*|--depth)
 			[[ $1 =~ ^\-[a-z].+$ ]] && opt_depth="${1:2}" || { opt_depth=$2; shift; }
 			[[ ! $opt_depth =~ ^[0-9]*$ ]] && ERROR "invalid depth" 1
-		;;
-		-f|--filenames) opt_filenames=1 ;;
-		-i|--ignore-case) opt_ignore_case=1 ;;
-		-p*|--path) [[ $1 =~ ^\-[a-z].+$ ]] && opt_path="${1:2}" || { opt_path=$2; shift; } ;;
+			;;
+		-f|--filenames)
+			opt_filenames=1 ;;
+		-i|--ignore-case)
+			opt_ignore_case=1 ;;
+		-p*|--path)
+			[[ $1 =~ ^\-[a-z].+$ ]] && opt_path="${1:2}" || { opt_path=$2; shift; } ;;
+
 		--) shift; break ;;
 		-*|--*) ERROR "unknown option ${1}" 1 ;;
 		*) break ;;
@@ -45,24 +51,19 @@ while (($#)); do
 	shift
 done
 
-[[ ! -d "$opt_path" ]] && ERROR "invalid path" 1
-
 [[ ! $1 ]] && { usage; exit 0; }
 
+[[ ! -d "$opt_path" ]] && ERROR "invalid path" 1
+
 grepopts="--with-filename --line-number --color=auto"
-findopts=""
+fopts="-type f"
 
 [[ $opt_binary ]] && grepopts="${grepopts} --binary-files=text" || grepopts="${grepopts} --binary-files=without-match"
 [[ $opt_filenames ]] && grepopts="${grepopts} -l"
 [[ $opt_ignore_case ]] && grepopts="${grepopts} -i"
-[[ $opt_depth ]] && findopts="${findopts} -maxdepth ${opt_depth}"
+[[ $opt_depth ]] && fopts="${fopts} -maxdepth ${opt_depth}"
 
-find "$opt_path" -type f \
-	-not -path '*/.Trash/*' \
-	-not -path '*/.Trashes/*' \
-	-not -path '*lost+found/' \
-	$findopts \
-	-print0 \
-	| xargs -0 -n 100 grep $grepopts "$@" 2>/dev/null
+find "$opt_path" $fopts \( -name .Trash -o -name .Trashes -o -name 'lost+found' \) -prune -o -type f -print0 |
+	xargs -0 -n 100 grep $grepopts "$@" 2>/dev/null
 
 exit 0
