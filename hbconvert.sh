6b7004f53aa3e5bfdb26469990abb6af20824219
Scott Buchanan <buchanan.sc@gmail.com>
2013-02-27 16:06:29 -0800

    no message

--- hbconvert.sh
+++ hbconvert.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+while [[ "$1" =~ ^- && ! -e "$1" ]]; do
+	flags="$flags $1"
+	shift
+done
+
 while (($#)); do
 	input="$1"
 	shift
 	[[ ! -f "$input" ]] && continue
 	output="${input%.*}.mp4"
-	[[ -e "$output" ]] && { echo "$output: file exists"; exit 1; }
-	nice HandBrakeCLI --preset "Normal" -O -i "$input" -o "$output"
+	[[ -e "$output" ]] && { echo "$output: file exists" >&2; exit 1; }
+	HandBrakeCLI --preset "Normal" -O -i "$input" -o "$output" $flags
 done
