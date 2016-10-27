52352d444a920ba6556d0ec553a1d5c2dfcde70d
Scott Buchanan <buchanan.sc@gmail.com>
2016-10-26 21:12:23 -0700

    oh dang what

--- strip-exif.php
+++ strip-exif.php
@@ -6,7 +6,7 @@
  * @author    Scott Buchanan <buchanan.sc@gmail.com>
  * @copyright 2016 Scott Buchanan
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
- * @version   r1 2016-10-19
+ * @version   r2 2016-10-26
  * @link      http://wafflesnatcha.github.com
  */
 require_once ('CLIScript.php');
@@ -17,10 +17,24 @@ $script = new CLIScript(array(
 	'usage' => '[FILE]'
 ));
 
-$image = array_pop($_SERVER['argv']);
+$args = $script->parseArgs();
 
-try {   
-	$res = imagecreatefromjpeg($image);
+if($_SERVER['argc'] <= 1) {
+	$script->usage();
+	exit;
+} else
+	array_shift($_SERVER['argv']);
+
+$file = array_pop($_SERVER['argv']);
+
+if(!is_file($file))
+	die("No file found\n");
+
+if(exif_imagetype($file) != IMAGETYPE_JPEG)
+	die("Not a JPEG image\n");
+
+try {
+	$res = imagecreatefromjpeg($file);
 	imagejpeg($res);
 } catch(Exception $e) {
    	echo 'Exception caught: ',  $e->getMessage(), "\n";
