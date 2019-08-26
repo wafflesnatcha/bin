1eeaab19467fae5527bbf6870a27e18d87383b21
Scott Buchanan <buchanan.sc@gmail.com>
2019-08-25 18:59:36 -0700

    Update watch-clipboard.sh
    added support for getclip (cygwin)

--- bash.d/watch-clipboard.sh
+++ bash.d/watch-clipboard.sh
@@ -4,10 +4,14 @@
 # Watch the clipboard for changes and display them to stdout. INTERVAL is the 
 # frequency (in seconds) to check the clipboard for changes.
 watch-clipboard() {
-	type -a xclip pbpaste &>/dev/null ||
-		{ echo "watch-clipboard: requires either \`xclip\` or \`pbcopy\`" 1>&2; return 2; }
-
 	local cmd current last _i=0.25
+
+	# determine clipboard command
+	type xclip &>/dev/null && cmd="xclip -o"
+	type pbpaste &>/dev/null && cmd="pbpaste"
+	type getclip &>/dev/null && cmd="getclip"
+
+	[[ -z "$cmd" ]] && { echo "watch-clipboard: requires \`xclip\`, \`pbcopy\`, or \`getclip\`" 1>&2; return 2; }
 
 	[[ $1 && ! $1 =~ ^[0-9]?(\.?[0-9]+)$ ]] && cat <<-EOF 1>&2 && return 1
 		Usage: watch-clipboard [INTERVAL]
@@ -18,7 +22,6 @@ watch-clipboard() {
 		EOF
 
 	_i=${1:-$_i}
-	type xclip &>/dev/null && cmd="xclip -o" || cmd="pbpaste"
 
 	while true; do
 		current="$($cmd)"
